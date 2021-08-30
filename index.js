17d9da275d0d02096d2afb37b4e19fab28eaee25
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-08-30 19:07:52 +0000

    solution

--- index.js
+++ index.js
@@ -4,5 +4,17 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-
+    let sm = nums1
+    let lg = nums2
+    if (nums1.length>nums2.length){
+        sm = nums2
+        lg = nums1
+    }
+    const result = []
+    sm.forEach(num => {
+    if (lg.filter(el => el===num).length>0 && result.filter(el => el===num).length<lg.filter(el => el===num).length){
+        result.push(num)
+        }
+    })
+    return result
 };
